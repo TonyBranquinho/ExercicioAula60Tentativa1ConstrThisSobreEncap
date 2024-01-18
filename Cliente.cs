2eb31e1373a574abd210ecb263efcdc758fcc55b
Tony <antony.branquinho@gmail.com>
2024-01-18 16:47:07 -0300

    bem desafiador

---
+++ Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioAula60Tentativa1ConstrutoresThisSobrecargaEncapsulamento {
+    internal class Cliente {
+
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        private int _numeroConta;
+        private string _titularConta;
+        private char _haveraDeposito;
+        private double _depositoInicial;
+        private double _saldo;
+
+        public Cliente (int numeroConta, string titularConta, char haveraDeposito, double depositoInicial, double saldo) {
+            _numeroConta = numeroConta;
+            _titularConta = titularConta;
+            _haveraDeposito = haveraDeposito;
+            _depositoInicial = depositoInicial;
+            _saldo = saldo;
+        }
+        
+        
+        public void Deposito(double deposito) {
+            _saldo = _saldo + deposito;
+        }
+        public void Saque(double saque) {
+            _saldo = _saldo - saque - 5.00;
+        }
+
+        public override string ToString() {
+            return "Conta "
+                + _numeroConta
+                + ", Titular: "
+                + _titularConta
+                + ", Saldo: $ "
+                + _saldo.ToString("F2", CI);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using ExercicioAula60Tentativa1ConstrutoresThisSobrecargaEncapsulamento;
+using System;
+using System.Globalization;
+
+namespace MyApp //ExercicioAula60Tentativa1ConstrutoresThisSobrecargaEncapsulamento.
+{
+    internal class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            
+            Console.Write("Digite o titular da conta: ");
+            string titularConta = Console.ReadLine();
+            
+            Console.Write("Haverá depósito inicial (s / n) ? ");
+            char haveraDeposito = char.Parse(Console.ReadLine());
+
+            double depositoInicial = 0;
+            if (haveraDeposito == 's') {
+                Console.Write("Digite o valor de depósito inicial: ");
+                depositoInicial = double.Parse(Console.ReadLine(), CI);
+            }
+
+            double saldo = depositoInicial;
+
+
+
+            Cliente cli = new Cliente(numeroConta, titularConta, haveraDeposito, depositoInicial, saldo);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(cli.ToString());
+
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para deposito: ");
+            double dep = double.Parse(Console.ReadLine(), CI);
+            cli.Deposito(dep);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cli.ToString());
+
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: ");
+            double saq = double.Parse(Console.ReadLine(), CI);
+            cli.Saque(saq);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(cli.ToString());
+
+
+        }
+    }
+}
+
+
+
+
